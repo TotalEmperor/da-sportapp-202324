cf45b5c9306de4d3c11b3ef04973d800f124b5aa
Nico Pasching <nico.pasching@stud.it.htl-hl.ac.at>
2023-08-26 16:44:38 +0200

    responsive width fix

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -17,8 +17,7 @@ export default function page() {
             <Head>
                 <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="icon"/>
             </Head>
-            <div
-                className="w-full flex flex-col sm:flex-row flex-wrap sm:flex-nowrap py-4 flex-grow sm:h-full">
+            <div className="w-full flex flex-col sm:flex-row flex-wrap sm:flex-nowrap py-4 flex-grow">
                 <div className={styles["w-left-fixed"] + " w-full flex-shrink flex-grow-0 px-4 "}>
                     <div
                         className="sticky top-0 sm:p-4 rounded-xl w-full h-full bg-[#F3F6EB] border-solid border-gray-300 border-2 min-w-fit">
@@ -61,13 +60,12 @@ export default function page() {
                     </div>
                 </div>
                 <main role="main" className={styles["w-center-fixed"] + " w-full flex-grow pt-1 px-3"}>
-                    <h1 className="text-3xl md:text-5xl mb-4 font-extrabold" id="home">Test Layout</h1>
+                    <h1 className="text-3xl md:text-5xl mb-4 font-extrabold" id="home">The Holy Grail Layout</h1>
                 </main>
-                <div className={styles["w-right-fixed"] + " w-full px-2 p-4 h-full"}>
+                <div className={styles["w-right-fixed"] + " w-full flex-shrink flex-grow-0 px-2"}>
                     <div className="flex sm:flex-col px-2">
                         <div className="bg-gray-50 rounded-xl border mb-3 w-full">
-                            <div
-                                className="max-w-7xl mx-auto py-8 px-4 sm:px-6 lg:py-12 lg:px-8 lg:flex lg:items-center lg:justify-between">
+                            <div className="max-w-7xl mx-auto py-8 px-4 sm:px-6 lg:py-12 lg:px-8 lg:flex lg:items-center lg:justify-between">
                                 <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
                                     <span className="block text-indigo-600">Made with Tailwind CSS!</span>
                                 </h2>
@@ -75,8 +73,7 @@ export default function page() {
                         </div>
                         <div className="p-2"></div>
                         <div className="bg-gray-100 rounded-xl mb-3 w-full">
-                            <div
-                                className="max-w-7xl mx-auto py-8 px-4 sm:px-6 lg:py-12 lg:px-8 lg:flex lg:items-center lg:justify-between">
+                            <div className="max-w-7xl mx-auto py-8 px-4 sm:px-6 lg:py-12 lg:px-8 lg:flex lg:items-center lg:justify-between">
                                 <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
                                     <span className="block">Ready to dive in?</span>
                                 </h2>
@@ -84,8 +81,7 @@ export default function page() {
                         </div>
                         <div className="p-2"></div>
                         <div className="bg-gray-50 rounded-xl border mb-3 w-full">
-                            <div
-                                className="max-w-7xl mx-auto py-8 px-4 sm:px-6 lg:py-12 lg:px-8 lg:flex lg:items-center lg:justify-between">
+                            <div className="max-w-7xl mx-auto py-8 px-4 sm:px-6 lg:py-12 lg:px-8 lg:flex lg:items-center lg:justify-between">
                                 <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
                                     <span className="block text-indigo-600">Play free at Codeply today.</span>
                                 </h2>
