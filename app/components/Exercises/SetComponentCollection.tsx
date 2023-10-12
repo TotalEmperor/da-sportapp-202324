2a8b2d0ec121333d7035e5a0c1985bcbd5adb4c2
Nico Pasching <nico.pasching@stud.it.htl-hl.ac.at>
2023-10-12 23:39:23 +0200

    Pulse Animation for Exercises

--- app/components/Exercises/SetComponentCollection.tsx
+++ app/components/Exercises/SetComponentCollection.tsx
@@ -48,11 +48,29 @@ export default function SetComponentCollection() {
 
     return (
         <>
-            {
+            {userdata.length==0 ?
+                <>
+                    <div className="border border-blue-300 shadow rounded-md p-4 max-w-sm w-full mx-auto">
+                        <div className="animate-pulse flex space-x-4">
+                            <div className="rounded-full bg-slate-200 h-10 w-10"></div>
+                            <div className="flex-1 space-y-6 py-1">
+                                <div className="h-2 bg-slate-200 rounded"></div>
+                                <div className="space-y-3">
+                                    <div className="grid grid-cols-3 gap-4">
+                                        <div className="h-2 bg-slate-200 rounded col-span-2"></div>
+                                        <div className="h-2 bg-slate-200 rounded col-span-1"></div>
+                                    </div>
+                                    <div className="h-2 bg-slate-200 rounded"></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </>
+                : (
                 userdata.map((data, index) => (
-                    <SetComponent key={index} data={data}/>
+                    <SetComponent key={index} data={data} />
                 ))
-            }
+            )}
         </>
     )
 }
@@ -65,8 +83,9 @@ const getExercises = async (data: any) => {
         for (const exerciseType in data.exercises[week][day]) {
             const exercises = data.exercises[week][day][exerciseType];
             objArray.push(exercises)
+            return objArray
         }
     }
-    return objArray;
+    return null;
 };
 
