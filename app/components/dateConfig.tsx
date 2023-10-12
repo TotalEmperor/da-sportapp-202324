58e6fc2acebf64395395fcb5bb0bb718c1b16709
TotalEmperor <nico.pasching26@gmail.com>
2023-10-12 19:55:13 +0200

    SideNav AccountButton DropUp

--- app/components/dateConfig.tsx
+++ app/components/dateConfig.tsx
@@ -1,19 +1,26 @@
 "use client"
 import CheckCircleOutlineIcon from "@mui/icons-material/CheckCircleOutline";
-import styles from "../(routes)/home/home.module.css";
 import RadioButtonUncheckedIcon from "@mui/icons-material/RadioButtonUnchecked";
-import React, {useState} from "react";
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+import React, {useEffect, useState} from "react";
+import {week} from "@/components/Exercises/SetComponentCollection";
 
+export default function DateConfig() {
 
-export default function dateConfig() {
+    const days = ["Mo", "Th", "We", "Tu", "Fr", "Sa", "Su"]
+    const [checked, setChecked] = useState<number | null>(null);
+    const [currentWeek, setCurrentWeek]=useState(String);
 
-    // eslint-disable-next-line react-hooks/rules-of-hooks
-    const [checked, setChecked] = useState<boolean[]>([]);
+    useEffect(() => {
+        setCurrentWeek(week)
+    }, [week]);
 
     const handleClick = (i: number) => {
-        const newCheckedState = [...checked];
-        newCheckedState[i] = !newCheckedState[i];
-        setChecked(newCheckedState);
+        if (checked === i) {
+            setChecked(null);
+        } else {
+            setChecked(i);
+        }
     };
 
     return (
@@ -21,21 +28,22 @@ export default function dateConfig() {
             <div className="rounded-xl border-2 border-[#9a9d93] w-[70%] min-w-fit">
                 <div className="w-fit justify-center flex-col mx-auto flex mb-3 px-4 pt-8 py-4">
                     <div className="flex w-fit ">
-                        <span className="font-bold text-3xl">22.5-29.5</span>
+                        <span className="font-bold text-3xl">{week}</span>
                     </div>
                     <div className="flex flex-row">
-                        {Array.from({length: 7}, (_, i) => (
-                            <div key={i} className="flex flex-col pe-3">
+                        {days.map((day, index) => (
+                            <div key={index} className="flex flex-col pe-3">
                                 <div
-                                    onClick={() => handleClick(i)}
-                                    className="cursor-pointer text-blue-500 hover:text-blue-700 flex justify-center"
+                                    onClick={() => handleClick(index)}
+                                    className="cursor-pointer hover:text-blue-700 flex justify-center"
                                 >
-                                    {checked[i] ? <CheckCircleOutlineIcon
-                                            className={styles["icons"] + " fill-[#1f1f1f] hover:fill-[#9a9d93] flex justify-center"}/> :
-                                        <RadioButtonUncheckedIcon
-                                            className={styles["icons"] + " fill-[#9a9d93] hover:fill-[#d9e7cb] flex justify-center items-center"}/>}
+                                    {checked === index ? (
+                                        <CheckCircleIcon sx={{ fontSize: '3rem', color: checked ? 'blue' : 'inherit' }} />
+                                    ) : (
+                                        <RadioButtonUncheckedIcon sx={{ fontSize: '3rem' }} />
+                                    )}
                                 </div>
-                                <h2 className="flex justify-center">Mon</h2>
+                                    <h2 key={index} className="flex justify-center">{day}</h2>
                             </div>
                         ))}
                     </div>
