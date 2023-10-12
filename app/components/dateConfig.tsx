d2b0d95b73362f367d6789a2a3b18f0063d8fb84
Nico Pasching <nico.pasching@stud.it.htl-hl.ac.at>
2023-10-12 23:25:40 +0200

    I wanna punch somebody out of frustration, but hey it works

--- app/components/dateConfig.tsx
+++ app/components/dateConfig.tsx
@@ -3,25 +3,53 @@ import CheckCircleOutlineIcon from "@mui/icons-material/CheckCircleOutline";
 import RadioButtonUncheckedIcon from "@mui/icons-material/RadioButtonUnchecked";
 import CheckCircleIcon from '@mui/icons-material/CheckCircle';
 import React, {useEffect, useState} from "react";
-import {week} from "@/components/Exercises/SetComponentCollection";
+import {getAuth} from "firebase/auth";
+import getFirestoreDocument from "@/firebase/firestore/getData";
+import {router} from "next/client";
+import {useRouter} from "next/navigation";
+
+export let week:string;
+export let day:string;
 
 export default function DateConfig() {
 
     const days = ["Mo", "Th", "We", "Tu", "Fr", "Sa", "Su"]
     const [checked, setChecked] = useState<number | null>(null);
-    const [currentWeek, setCurrentWeek]=useState(String);
+    const [currentWeek, setCurrentWeek] = useState()
+    const user = getAuth().currentUser.uid;
+    const router = useRouter();
 
+// keeps `userdata` up to date
     useEffect(() => {
-        setCurrentWeek(week)
-    }, [week]);
+
+        getFirestoreDocument("exercises", user).then((res: any) => {
+            if (res.result) {
+                const dates = sortDates(Object.keys(res.result.exercises)).then((date)=>{
+                    setCurrentWeek(date[0])
+                    week = date[0];
+                    router.refresh()
+                })
+            }
+        });
+    }, [user]); // <-- rerun when user changes
 
     const handleClick = (i: number) => {
         if (checked === i) {
+            day = days[i].toUpperCase()
             setChecked(null);
         } else {
+            day = days[i].toUpperCase()
             setChecked(i);
         }
+        router.refresh()
     };
+
+    useEffect(() => {
+        if (!user || !week) {
+            // user still loading, do nothing yet
+            return;
+        }
+    }, [week]);
 
     return (
         <>
@@ -34,13 +62,12 @@ export default function DateConfig() {
                         {days.map((day, index) => (
                             <div key={index} className="flex flex-col pe-3">
                                 <div
-                                    onClick={() => handleClick(index)}
                                     className="cursor-pointer hover:text-blue-700 flex justify-center"
                                 >
                                     {checked === index ? (
-                                        <CheckCircleIcon sx={{ fontSize: '3rem', color: checked ? 'blue' : 'inherit' }} />
+                                        <CheckCircleIcon onClick={() => handleClick(index)} sx={{ fontSize: '3rem', color: "#2ECCFA" }} />
                                     ) : (
-                                        <RadioButtonUncheckedIcon sx={{ fontSize: '3rem' }} />
+                                        <RadioButtonUncheckedIcon onClick={() => handleClick(index)} sx={{ fontSize: '3rem' }} />
                                     )}
                                 </div>
                                     <h2 key={index} className="flex justify-center">{day}</h2>
@@ -53,4 +80,20 @@ export default function DateConfig() {
 
         </>
     )
+}
+
+
+const sortDates = async (dates:any)=>{
+    dates.sort((a, b) => {
+        // Split the string and take the first part as the starting date of the week
+        const date1 = new Date(convertDateFormat(a.split('-')[0]));
+        const date2 = new Date(convertDateFormat(b.split('-')[0]));
+        return date1.getTime() - date2.getTime();
+    });
+    return dates;
+}
+
+function convertDateFormat(date: string): string {
+    const [day, month, year] = date.split('.');
+    return `${month}/${day}/${year}`;
 }
