2b51aff3f2018784218527a816be46f85bb14d46
Nico Pasching <nico.pasching@stud.it.htl-hl.ac.at>
2023-10-12 23:55:06 +0200

    Pulse Animation for Exercises

--- app/components/Exercises/SetComponentCollection.tsx
+++ app/components/Exercises/SetComponentCollection.tsx
@@ -12,16 +12,11 @@ export default function SetComponentCollection() {
         return getAuth().currentUser.uid || undefined;
     });
     const [userdata, setuserdata] = useState([]);
-    const [currentDay, setCurrentDay] = useState(day);
-    const [currentWeek, setCurrentWeek] = useState(week);
-    const router = useRouter();
 
 
 // keeps `userdata` up to date
 
     useEffect(() => {
-        setCurrentDay(day)
-        setCurrentWeek(week)
         if (user === null) {
             setuserdata(null); // <-- clear data when not logged in
             return;
@@ -48,13 +43,13 @@ export default function SetComponentCollection() {
 
     return (
         <>
-            {userdata.length==0 ?
+            {userdata.length == 0 ?
                 <>
                     <div className="border border-blue-300 shadow rounded-md p-4 max-w-sm w-full mx-auto">
                         <div className="animate-pulse flex space-x-4">
                             <div className="rounded-full bg-slate-200 h-10 w-10"></div>
                             <div className="flex-1 space-y-6 py-1">
-                                <div className="h-2 bg-slate-200 rounded"></div>
+                                <div className="h-2 bg-slate-200 rounded opacity-50"></div>
                                 <div className="space-y-3">
                                     <div className="grid grid-cols-3 gap-4">
                                         <div className="h-2 bg-slate-200 rounded col-span-2"></div>
@@ -67,10 +62,10 @@ export default function SetComponentCollection() {
                     </div>
                 </>
                 : (
-                userdata.map((data, index) => (
-                    <SetComponent key={index} data={data} />
-                ))
-            )}
+                    userdata.map((data, index) => (
+                        <SetComponent key={index} data={data}/>
+                    ))
+                )}
         </>
     )
 }
@@ -79,7 +74,7 @@ const getExercises = async (data: any) => {
 
     let objArray = [];
 
-    if(day!=null){
+    if (day != null) {
         for (const exerciseType in data.exercises[week][day]) {
             const exercises = data.exercises[week][day][exerciseType];
             objArray.push(exercises)
