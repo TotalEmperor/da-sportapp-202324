505e0ad95f45b44083f3351dfea2e045c58d52c7
TotalEmperor <nico.pasching26@gmail.com>
2023-10-20 11:32:52 +0200

    s

--- app/(routes)/modifying/page.tsx
+++ app/(routes)/modifying/page.tsx
@@ -1,7 +1,6 @@
 import Head from "next/head";
 import SideNav from "@/components/SideNav";
 import MainComponent from "@/components/MainScreen/mainComponent";
-import SetComponent from "@/components/MainScreen/SetComponent";
 import ConfPanel from "@/components/confPanel";
 import DateConfig from "@/components/dateConfig";
 import React from "react";
@@ -17,7 +16,8 @@ export default function page(){
             <div className="w-full flex flex-col sm:flex-row flex-wrap sm:flex-nowrap py-4 flex-grow">
                 <SideNav/>
                 <MainComponent>
-                    <SetComponent/>
+                    <>
+                    </>
                 </MainComponent>
                 <ConfPanel>
                     <>
