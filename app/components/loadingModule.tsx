d9be3942d4136c0e0ceef14d0240cd2845c2444b
TotalEmperor <nico.pasching26@gmail.com>
2023-11-24 16:13:38 +0100

    Modify improvements

--- app/components/loadingModule.tsx
+++ app/components/loadingModule.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function LoadingModule(){
     return (
         <>
-            <div className="border border-blue-300 shadow rounded-md md:p-4 sm:w-[80%] h-[10rem]">
+            <div className="border border-blue-300 shadow rounded-md md:p-4 md:w-[80%] h-[10rem]">
                 <div className="animate-pulse flex space-x-4">
                     <div className="rounded-full bg-slate-200 h-10 w-10"></div>
                     <div className="flex-1 space-y-6 py-1">
